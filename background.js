e6121df9e53597ccbc270c92a5add02ad8aff031
Prasanna Kulkarni <prasanna.kkarni@gmail.com>
2026-06-03 18:16:08 +0530

    red ball moving right

--- background.js
+++ background.js
@@ -4,5 +4,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 200, 200);
+let x = 100;
+let y = 100;
+
+function animate() {
+
+    // Clear previous frame
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Move ball
+    x += 2;
+
+    // Draw ball
+    ctx.beginPath();
+    ctx.arc(x, y, 30, 0, 2 * Math.PI);
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    // Request next frame
+    requestAnimationFrame(animate);
+}
+
+animate();
