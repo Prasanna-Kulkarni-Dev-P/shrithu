47cd8424ff5c5fa8e3ffd8c7a1438d19b283e802
Prasanna Kulkarni <prasanna.kkarni@gmail.com>
2026-06-03 18:08:12 +0530

    Add background.js to draw a circle on canvas

---
+++ background.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById("background");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.beginPath();
+ctx.arc(200, 150, 30, 0, Math.PI * 2);
+ctx.fill();
