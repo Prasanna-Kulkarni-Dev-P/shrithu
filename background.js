55b0e463c6ab405cdeffb9026e3519676e24c10a
Prasanna Kulkarni <prasanna.kkarni@gmail.com>
2026-06-03 18:14:12 +0530

    Replace circle drawing with rectangle drawing

--- background.js
+++ background.js
@@ -4,8 +4,5 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.beginPath();
-ctx.arc(200, 150, 30, 0, Math.PI * 2);
-
-ctx.fillStyle = "red";     // Change color here
-ctx.fill();
+ctx.fillStyle = "red";
+ctx.fillRect(50, 50, 200, 200);
